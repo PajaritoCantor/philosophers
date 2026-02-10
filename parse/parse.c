0d827f2e112fe565dbc9ca39da01d186301b9c24
Juan David Rodriguez Laverde <jurodrig@c1r3s3.42madrid.com>
2026-02-10 11:14:46 +0100

    parsing

--- parse/parse.c
+++ parse/parse.c
@@ -6,11 +6,16 @@
 /*   By: jurodrig <jurodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 15:34:29 by jurodrig          #+#    #+#             */
-/*   Updated: 2026/02/09 16:05:21 by jurodrig         ###   ########.fr       */
+/*   Updated: 2026/02/10 11:14:35 by jurodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+static inline bool	is_digit(char c)
+{
+	return (c >= 48 && c <= 57)
+}
 
 static inline bool	is_space(char c)
 {
@@ -25,9 +30,18 @@ static const char	*valid_input(const char *str)
 	len = 0;
 	while (is_space(*str))
 		++str;
-	if (*str == '+')
+	if (*str == 43)
 		++str;
-	else if ()
+	else if (*str == 45)
+		error_exit("Feed me only positive valuse u suck!");
+	if (!is_digit(*str))
+		error_exit("The input is not a correct digit");
+	number = str;
+	while (is_digit(*str++))
+		++len;
+	if (len > 10)
+		error_exit("The value is too big, INT_MAX is the limit");
+	return (number);
 }
 static long	ft_atol(const char *str)
 {
@@ -35,10 +49,17 @@ static long	ft_atol(const char *str)
 
 	num = 0;
 	str = valid_input(str);
+	while (is_digt(*str))
+		num = (num * 10) + (*str++ - 48);
+	if (num > INT_MAX)
+		error_exit("The value is too big, INT_MAX is the limit!");
+	return (num);
 }
 
 void    parse_input(t_table *table, char **av)
 {
 	table->philo_nbr = ft_atol(av[1]);
-	
+	table->time_to_die = ft_atol(av[2]) * 1e3;
+	table->time_to_eat = ft_atol(av[3]) * 1e3;
+	table->time_to_sleep = ft_atol(av[4]) * 1e3;
 }
